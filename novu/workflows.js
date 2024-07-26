f94c60e8a1afc813a703abdb56a5c13207e441f1
NIKHIL <nikhilharisinghani26@gmail.com>
2024-07-26 16:20:24 +0530

    init

---
+++ novu/workflows.js
@@ -0,0 +1,37 @@
+const { workflow } = require('@novu/framework');
+const { z } = require('zod');
+const testWorkflow = workflow('test-workflow', async ({ step, payload }) => {
+    await step.email('send-email', async (controls) => {
+        return {
+            subject: controls.subject,
+            body: 'This is your first Novu Email ' + payload.userName,
+        };
+    },
+        {
+            controlSchema: z.object({
+                subject: z.string().default('A Successful Test on Novu from {{userName}}'),
+            }),
+        });
+}, {
+    payloadSchema: z.object({
+        userName: z.string().default('John Doe'),
+    }),
+});
+
+const pushNotificationWorkflow = workflow(
+    "push-notification-workflow",
+    async ({ step, payload }) => {
+        await step.push("send-push", async () => {
+            return {
+                subject: "Hello world",
+                title: "New Notification",
+                content: `Hello ${payload.name}, you have a new notification!`,
+                body: "Droop"
+            };
+        });
+    },
+    { payloadSchema: z.object({ name: z.string() }) }
+);
+
+
+module.exports = { testWorkflow, pushNotificationWorkflow };
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { serve } = require("@novu/framework/express");
+// require('dotenv').config();
+const { testWorkflow, pushNotificationWorkflow } = require("./novu/workflows");
+const app = express();
+app.use(express.json()); // Required for Novu POST requests
+
+app.use("/api/novu", serve({ workflows: [testWorkflow, pushNotificationWorkflow] }));
+
+app.listen(4000);
