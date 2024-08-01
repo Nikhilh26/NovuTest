e35fb97ab1fa88c871cec5e8f9d66d141c2b33ed
NIKHIL <nikhilharisinghani26@gmail.com>
2024-08-01 16:26:21 +0530

    Test commit 5

--- novu/workflows.js
+++ novu/workflows.js
@@ -1,38 +1,19 @@
 const { workflow } = require('@novu/framework');
 const { z } = require('zod');
 
-const testWorkflow = workflow('test-workflow', async ({ step, payload }) => {
-    await step.email('send-email', async (controls) => {
-        return {
-            subject: controls.subject,
-            body: 'This is your first Novu Email ' + payload.userName,
-        };
-    },
-        {
-            controlSchema: z.object({
-                subject: z.string().default('A Successful Test on Novu from {{userName}}'),
-            }),
-        });
-}, {
-    payloadSchema: z.object({
-        userName: z.string().default('John Doe'),
-    }),
-});
-
 const pushNotificationWorkflow = workflow(
     "push-notification-workflow",
     async ({ step, payload }) => {
-        console.log(payload + "data sent");
         await step.inApp("send-push", async () => {
             return {
                 subject: "Hello world",
-                title: "New Notification",
+                title: "New Notifisscation",
                 content: `Hello ${payload.name}, you have a new notification!`,
                 body: `${payload.name} hello`
             };
         });
     },
-    { payloadSchema: z.object({ name: z.string() }) }
+    { payloadSchema: z.object({ name: z.string(), title: z.string() }) }
 );
-// headless
-module.exports = { testWorkflow, pushNotificationWorkflow };
+
+module.exports = { pushNotificationWorkflow }
